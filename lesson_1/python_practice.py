6cdb6d3344b7b6ed0d01491c62433d426c0a5819
Thohidul Islam <thohidu.islam@gmail.com>
2020-07-19 22:00:55 +0600

    bit better solution quiz python practice of lesson 1

--- lesson_1/python_practice.py
+++ lesson_1/python_practice.py
@@ -25,15 +25,14 @@ class Classy(object):
 
     def getClassiness(self):
         classiness = 0
-        for item in self.items:
-            if item == 'tophat':
-                classiness += 2
-            elif item =='bowtie':
-                classiness += 4
-            elif item == 'monocle':
-                classiness += 5 
-            else:
-                classiness += 0
+        if len(self.items) > 0:            
+            for item in self.items:
+                if item == 'tophat':
+                    classiness += 2
+                elif item =='bowtie':
+                    classiness += 4
+                elif item == 'monocle':
+                    classiness += 5 
         return classiness
 
 
