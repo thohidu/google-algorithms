27b45db70e02ab7785de4ba62a8e76c2c4eb9427
Thohidul Islam <thohidu.islam@gmail.com>
2020-08-10 18:31:25 +0600

    add recursion visualiztion

--- lesson_3/recursion_quiz.py
+++ lesson_3/recursion_quiz.py
@@ -11,27 +11,31 @@ def get_fib(position):
     else:
         return get_fib(position - 1) + get_fib(position - 2)
 
-
-
-"""position 2
-                    get_fibo(1)       +       get_fibo(0)
-                        1             +            0
-                        return        1
-
-   position 3
-                    get_fibo(2)                  +                   get_fibo(1)
-            get_fibo(1)+ get_fibo(0)             +                       1    
-                 1     +    0
-                                                 2
-    
-      
-"""
-
-
 if __name__ == "__main__":
     # Test cases 
     print(get_fib(9))
     print(get_fib(11))
     print(get_fib(0))
+
+
+"""position 2
+                    get_fib(1)                  +                   get_fib(0)
+                        1                       +                        0
+            return                              1
+
+   position 3
+                    get_fib(2)                  +                   get_fib(1)
+            get_fib(1) + get_fib(0)             +                       1    
+                 1     +    0                   
+            return                              2
+    
+    position 4
+                   get_fib(3)                   +                   get_fib(2)
+            get_fib(2)       +    get_fib(1)    +              get_fib(1) + get_fib(0)   
+    get_fib(1) + get_fib(0)  +       1                                1     +    0 
+         1     +  0         
+    return                                      3
+"""
+
     
 
