03c7c296006581a600acd3bce2b1be794f191559
Thohidul Islam <thohidu.islam@gmail.com>
2020-08-25 00:01:49 +0600

    add more tests

--- lesson_5/binary_tree_quiz.py
+++ lesson_5/binary_tree_quiz.py
@@ -54,9 +54,7 @@ if __name__ == "__main__":
     tree.root.left = Node(2)
     tree.root.right = Node(3)
     tree.root.left.left = Node(4)
-    tree.root.left.right = Node(5)
-    
- 
+    tree.root.left.right = Node(5)  
 
     # Test search 
     # Should be True
@@ -66,4 +64,24 @@ if __name__ == "__main__":
 
     # Test print_tree
     # Should be 1-2-4-5-3
+    print(tree.print_tree())
+
+    # Set up 2nd tree
+    tree = BinaryTree(1)
+    tree.root.left = Node(2)
+    tree.root.right = Node(3)
+    tree.root.left.left = Node(4)
+    tree.root.left.right = Node(5)
+    
+    tree.root.right.left = Node(6)
+    tree.root.right.right = Node(7) 
+
+    # Test search 
+    # Should be True
+    print(tree.search(4))
+    # Should be True
+    print(tree.search(6))
+
+    # Test print_tree
+    # Should be 1-2-4-5-3-6-7
     print(tree.print_tree())
